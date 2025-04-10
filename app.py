6071a6f93e6a3c8eb4c64ace238532285b0ad08c
Honghanhi <117724583+Honghanhi@users.noreply.github.com>
2025-04-10 18:44:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -105,4 +105,6 @@ def leave_room():
     return '', 204
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))  # lấy PORT từ môi trường, fallback là 5000
+    app.run(host='0.0.0.0', port=port)
